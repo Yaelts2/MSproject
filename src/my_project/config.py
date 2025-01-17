d431877e8df86b414889b8c71a13299061883e60
Yaelts2 <yaelts2@gmail.com>
2025-01-17 09:19:33 +0200

    Try Git

--- src/my_project/config.py
+++ src/my_project/config.py
@@ -1,6 +1,6 @@
 # Path to the folder containing your data
 #DATA_PATH
-DATA_PATH =["C:\project\Data\gandalf", "C:\project\Data\legolas"]
+DATA_PATH =["C:\myprojects\data\gandalf", "C:\myprojects\data\legolas"]
 
 
 # Other configuration variables
